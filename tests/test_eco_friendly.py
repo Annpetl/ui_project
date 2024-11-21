25298967c9190327d4c0137b1d358932eb8f05ec
Hanna <annpetlitskaya@gmail.com>
2024-11-21 10:52:43 +0100

    updated tests

--- tests/test_eco_friendly.py
+++ tests/test_eco_friendly.py
@@ -1,18 +1,16 @@
-from pages.eco_friendly_page import SORTER_LOC
+def test_sale_page_is_open(eco_friendly):
+    eco_friendly.open_page()
+    eco_friendly.check_page_endpoint_is_correct()
+    eco_friendly.check_page_title_is('Eco Friendly')
 
 
-def test_sale_page_is_open(eco_friendly_page):
-    eco_friendly_page.check_page_endpoint_is_correct()
-    eco_friendly_page.check_page_title_is('Eco Friendly')
+def test_sort_by_price(eco_friendly):
+    eco_friendly.open_page()
+    eco_friendly.select_by_value('price')
+    eco_friendly.check_sort_by_price()
 
 
-def test_sort_by_price(eco_friendly_page):
-    eco_friendly_page.click(SORTER_LOC)
-    eco_friendly_page.select_by_value('price')
-    eco_friendly_page.check_sort_by_price()
-
-
-def test_sort_by_name(eco_friendly_page):
-    eco_friendly_page.click(SORTER_LOC)
-    eco_friendly_page.select_by_value('name')
-    eco_friendly_page.check_sort_by_product_name()
+def test_sort_by_name(eco_friendly):
+    eco_friendly.open_page()
+    eco_friendly.select_by_value('name')
+    eco_friendly.check_sort_by_product_name()
