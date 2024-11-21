a24f1badc008b998a9695670aef3c9a1ecea92be
Hanna <annpetlitskaya@gmail.com>
2024-11-21 10:50:27 +0100

    updated methods

--- pages/base_page.py
+++ pages/base_page.py
@@ -20,12 +20,8 @@ class BasePage:
     def find_all(self, locator):
         return self.driver.find_elements(*locator)
 
-    def check_elem_text_is(self, locator, text):
-        assert self.find(locator).text == text
-
-    def check_elems_text_is(self, locator, title_list: list):
-        categoty_title_list = [elem.text for elem in self.find_all(locator)]
-        assert categoty_title_list == title_list
+    def get_text(self, locator):
+        return self.find(locator).text
 
     def check_page_title_is(self, title):
         assert self.driver.title == title
@@ -33,5 +29,3 @@ class BasePage:
     def check_page_endpoint_is_correct(self):
         assert self.driver.current_url.endswith(self.page_endpoint)
 
-    def check_length_of_elems(self, locator, length):
-        assert len(self.find_all(locator)) == length
