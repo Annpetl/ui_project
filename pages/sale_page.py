ac2024e8d81c3a8be521cf834aab255d56b42243
Hanna <annpetlitskaya@gmail.com>
2024-11-21 10:52:10 +0100

    added methods

--- pages/sale_page.py
+++ pages/sale_page.py
@@ -8,3 +8,9 @@ BLOCK_PROMO_ELEMS_LOC = (By.CSS_SELECTOR, "a.block-promo")
 class SalePage(BasePage):
     page_endpoint = '/sale.html'
 
+    def check_category_menu_titles_are(self, title_list: list):
+        categoty_title_list = [elem.text for elem in self.find_all(CATEGORY_MENU_TITLES_LOC)]
+        assert categoty_title_list == title_list
+
+    def check_block_promo_elems_count_is(self, count):
+        assert len(self.find_all(BLOCK_PROMO_ELEMS_LOC)) == count
