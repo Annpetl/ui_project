9786d0e161d3c4209689fc63721d45cd36ea486d
Hanna <annpetlitskaya@gmail.com>
2024-11-21 10:51:53 +0100

    updated methods

--- pages/eco_friendly_page.py
+++ pages/eco_friendly_page.py
@@ -1,4 +1,3 @@
-import time
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
@@ -15,9 +14,11 @@ class EcoFriendly(BasePage):
         Select(self.find(SORTER_LOC)).select_by_value(value)
 
     def check_sort_by_price(self):
+        self.click(SORTER_LOC)
         price_list = [float(price.text[1:]) for price in self.find_all(PRICE_LIST_LOC)]
         assert price_list == sorted(price_list)
 
     def check_sort_by_product_name(self):
+        self.click(SORTER_LOC)
         product_title_list = [product.text for product in self.find_all(PRODUCT_LIST_LOC)]
         assert product_title_list == sorted(product_title_list)
