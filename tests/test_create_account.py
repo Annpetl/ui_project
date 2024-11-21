700af9105db2a59567e4d77c1d6a4f721a27eb24
Hanna <annpetlitskaya@gmail.com>
2024-11-21 10:52:31 +0100

    updated tests

--- tests/test_create_account.py
+++ tests/test_create_account.py
@@ -1,24 +1,22 @@
-from pages.create_account_page import CREATE_ACCOUNT_BTN_LOC, PASSWORD_CONFIRM_ERROR_LOC, EMPTY_FIELD_ERROR_LOC
-from pages.customer_account_page import SUCCESS_CREATE_MSG_LOC
-
-
-def test_valid_body(create_account_page, customer_account, create_test_data):
-    create_account_page.fill_in_form(create_test_data['name'], create_test_data['last_name'], create_test_data['email'],
-                                     create_test_data['password'], create_test_data['password_confirm'])
+def test_successful_registration(create_account, customer_account, create_test_data):
+    create_account.open_page()
+    create_account.fill_in_form(create_test_data['name'], create_test_data['last_name'], create_test_data['email'],
+                                create_test_data['password'], create_test_data['password_confirm'])
     customer_account.check_page_title_is('My Account')
     customer_account.check_page_endpoint_is_correct()
-    customer_account.check_elem_text_is(SUCCESS_CREATE_MSG_LOC,
-                                        "Thank you for registering with Main Website Store.")
+    customer_account.check_success_create_msg_is("Thank you for registering with Main Website Store.")
 
 
-def test_empty_body(create_account_page):
-    create_account_page.find(CREATE_ACCOUNT_BTN_LOC).click()
-    create_account_page.check_length_of_elems(EMPTY_FIELD_ERROR_LOC, 5)
-    create_account_page.check_page_endpoint_is_correct()
+def test_empty_registration_form(create_account):
+    create_account.open_page()
+    create_account.click_account_button()
+    create_account.check_empty_field_messages_count_is(5)
+    create_account.check_page_endpoint_is_correct()
 
 
-def test_incorrect_confirm_password(create_account_page, create_test_data):
-    create_account_page.fill_in_form(create_test_data['name'], create_test_data['last_name'], create_test_data['email'],
-                                     create_test_data['password'], password_confirm='111111')
-    create_account_page.check_elem_text_is(PASSWORD_CONFIRM_ERROR_LOC, "Please enter the same value again.")
-    create_account_page.check_page_endpoint_is_correct()
+def test_incorrect_confirm_password(create_account, create_test_data):
+    create_account.open_page()
+    create_account.fill_in_form(create_test_data['name'], create_test_data['last_name'], create_test_data['email'],
+                                create_test_data['password'], password_confirm='111111')
+    create_account.check_password_confirm_error_msg_is("Please enter the same value again.")
+    create_account.check_page_endpoint_is_correct()
